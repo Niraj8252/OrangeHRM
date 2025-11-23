50597b60655038f6c5b2f7f6c6d0d4774df521ec
Niraj8252 <nirajkumar8252@gmail.com>
2025-11-23 13:16:57 +0530

    Login page changes

--- src/main/java/com/orangeHRM/elementRepository/LoginPage.java
+++ src/main/java/com/orangeHRM/elementRepository/LoginPage.java
@@ -6,8 +6,12 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
+	private WebDriver driver;
+
+
 	//Constructor
 		public LoginPage(WebDriver driver) {
+			this.driver = driver;
 			PageFactory.initElements(driver, this);
 		}
 		
